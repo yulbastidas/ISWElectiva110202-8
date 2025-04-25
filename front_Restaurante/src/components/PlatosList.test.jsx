78d862071a0d5010b0a528e2c4cf8878384edf1f
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-04-25 08:32:06 -0500

    correccion

--- front_Restaurante/src/components/PlatosList.test.jsx
+++ front_Restaurante/src/components/PlatosList.test.jsx
@@ -1,3 +1,4 @@
+//pruebas unitarias
 import React from 'react';
 import { render, screen, fireEvent, waitFor, within } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
