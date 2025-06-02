fdb5aa543168f6465612c67b57e43444a3748af0
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-06-02 11:19:34 -0500

    original

--- Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/urls.py
+++ Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
+
 
 
 
 urlpatterns = [  
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
+   
     path("admin/", admin.site.urls),
     path("api/restaurante/", include("AppRestaurante.urls")),
     path("api/usuarios/", include("AppUsuarios.urls")),
