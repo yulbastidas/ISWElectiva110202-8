192b96e99b8200c9cb0ba53edb38a24830b26d08
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-06-02 13:41:39 -0500

    ...

--- Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/settings.py
+++ Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/settings.py
@@ -118,7 +118,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    # Agregar aquí las URLs de tu frontend en producción
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
