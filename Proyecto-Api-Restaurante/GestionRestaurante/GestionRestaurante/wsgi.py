5673a9cddec45d2fd1b1549567b5a7289f51457f
yulbastidas <yuly.bastidas@campusucc.edu.co>
2025-06-02 12:58:32 -0500

    .

--- Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/wsgi.py
+++ Proyecto-Api-Restaurante/GestionRestaurante/GestionRestaurante/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GestionRestaurante.GestionRestaurante.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GestionRestaurante.settings")
+
 
 
 application = get_wsgi_application()
